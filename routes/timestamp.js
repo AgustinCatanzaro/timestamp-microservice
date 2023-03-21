1fde377cc533ff73cd1fd1d3a8b210656e93aa3d
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-03-21 12:32:40 -0300

    added date param to be optional

--- routes/timestamp.js
+++ routes/timestamp.js
@@ -3,6 +3,6 @@ const router = express.Router()
 
 const { getTimeStamp } = require('../controllers/timestamp')
 
-router.route('/:date').get(getTimeStamp)
+router.route('/:date?').get(getTimeStamp)
 
 module.exports = router
